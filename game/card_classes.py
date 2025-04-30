3dbed9b66a75ea023c33c0a09fc15a779fd820f5
Mr-Mark6133 <markus.vahlers@gmail.com>
2025-04-30 13:11:54 +0200

    fix this later

--- game/card_classes.py
+++ game/card_classes.py
@@ -1,22 +1,23 @@
 # Card superclass:
 class Card:
-    def __init__(self, category, name, manaCost, pic=None):
+    def __init__(self, category, name, manaCost, effect, pic=None):
         self.category = category
         self.name = name
         self.manaCost = manaCost
+        self.effect = effect
         self.pic = pic
 
 # Hero subclass:
 class Hero(Card):
     def __init__(self, name, attack, hp, pic=None):
-        super().__init__('hero', name, manaCost=0, pic=None)
+        super().__init__(category='hero', name, manaCost=0, effect=None, pic=pic)
         self.attack = attack
         self.hp = hp
 
 # Minion subclass:
 class Minion(Card):
     def __init__(self, name, manaCost, attack, hp, effect, pic=None):
-        super().__init__('minion', name, manaCost, pic)
+        super().__init__('minion', name, manaCost, effect, pic)
         self.attack = attack
         self.hp = hp
         self.effect = effect
@@ -24,10 +25,10 @@ class Minion(Card):
 # Spell subclass:
 class Spell(Card):
     def __init__(self, name, manaCost, pic=None):
-        super().__init__('spell', name, manaCost, pic)
+        super().__init__('spell', name, manaCost, effect=None, pic=pic)
 
 # Weapon subclass:
 class Weapon(Card):
     def __init__(self, name, manaCost, attack, durability, pic=None):
-        super().__init__('weapon', name, manaCost, pic)
+        super().__init__('weapon', name, manaCost, effect=None, pic=pic)
         self.attack = attack
--- game/card_data.py
+++ game/card_data.py
@@ -3,23 +3,23 @@ import effects_data as ed
 
 # Eksempel på minion kort:
 def someGuy():
-    return Minion("Some Guy", manaCost=3, attack=2, hp=5, effect=None)
+    return cc.Minion("Some Guy", manaCost=3, attack=2, hp=5, effect=None)
 
 def someCoolGuy():
     ed.increase(1, 0)
-    return Minion("Some Cool Guy", manaCost=5, attack=4, hp=6, effect="When summoned: increase all minions attack by 1.")
+    return cc.Minion("Some Cool Guy", manaCost=5, attack=4, hp=6, effect="When summoned: increase all minions attack by 1.")
 
 def tank():
     ed.armor(2)
     ed.taunt()
-    return Minion("Tank", manaCost=3, attack=1, hp=7, effect="When summoned: gain 2 armor. If summoned in the 1st row: Taunt.")
+    return cc.Minion("Tank", manaCost=3, attack=1, hp=7, effect="When summoned: gain 2 armor. If summoned in the 1st row: Taunt.")
     
 # Eksempel på spell kort:
 def fireball():
-    return Spell("Fireball", manaCost=4)
+    return cc.Spell("Fireball", manaCost=4)
 
 # Eksempel på Hero kort:
 def adventurer():
-    return Hero("Adventurer", attack=1, hp=15)
+    return cc.Hero("Adventurer", attack=1, hp=15)
 
 
