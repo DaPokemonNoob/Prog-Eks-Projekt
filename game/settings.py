76ba1f4fc05b4dff93064713378d5395ecc29f32
Marib05 <159001716+Marib05@users.noreply.github.com>
2025-05-07 23:32:58 +0200

    new files

---
+++ game/settings.py
@@ -0,0 +1,11 @@
+import pygame
+
+WIDTH, HEIGHT = 1280, 720
+SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
+
+# Card size constants
+CARD_WIDTH = 100
+CARD_HEIGHT = 140
+HERO_SCALE = 2
+HERO_CARD_WIDTH = int(CARD_WIDTH * HERO_SCALE)
+HERO_CARD_HEIGHT = int(CARD_HEIGHT * HERO_SCALE)
